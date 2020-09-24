ed38b885fc404cc49a6489803b101d047b98a518
hksdpc250 <43977088+hksdpc250@users.noreply.github.com>
2020-09-24 19:52:00 +0800

    Try to fix post build script. 4

--- diy-post.sh
+++ diy-post.sh
@@ -7,15 +7,9 @@ _vermagic="$(curl --retry 5 -L "https://downloads.openwrt.org/releases/${_versio
 OLD_CWD="$(pwd)"
 
 [ "$(find build_dir/ -name .vermagic -exec cat {} \;)" = "$_vermagic" ] && \
-mkdir ../openwrt-imagebuilder && \
-tar -xJpf bin/targets/ipq40xx/generic/openwrt-imagebuilder-${_version}-ipq40xx-generic.Linux-x86_64.tar.xz -C ../openwrt-imagebuilder && \
-( cd ../openwrt-imagebuilder/* || (
-    ls -all ../openwrt-imagebuilder/ ../openwrt-imagebuilder/* || true
-    whoami || true
-    groups || true
-) || true ) && \
-( cd ../openwrt-imagebuilder/* || sudo chmod 777 ../openwrt-imagebuilder ../openwrt-imagebuilder/* || true ) && \
-cd ../openwrt-imagebuilder/* && \
+mkdir openwrt-imagebuilder && \
+tar -xJf bin/targets/ipq40xx/generic/openwrt-imagebuilder-${_version}-ipq40xx-generic.Linux-x86_64.tar.xz -C openwrt-imagebuilder && \
+cd openwrt-imagebuilder/* && \
 make image PROFILE='p2w_r619ac' && \
 mv bin/targets/ipq40xx/generic/openwrt-${_version}-ipq40xx-generic-p2w_r619ac-squashfs-nand-factory.bin ../openwrt-${_version}-minimal-ipq40xx-generic-p2w_r619ac-squashfs-nand-factory.bin && \
 mv bin/targets/ipq40xx/generic/openwrt-${_version}-ipq40xx-generic-p2w_r619ac-squashfs-nand-factory.ubi ../openwrt-${_version}-minimal-ipq40xx-generic-p2w_r619ac-squashfs-nand-factory.ubi && \
